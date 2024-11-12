4dcd1a5eee45da7e2000ce1392c2ed127ee180e9
Saniya Atar <114806512+SaniyaAtar@users.noreply.github.com>
2024-11-12 19:53:37 +0530

    Add files via upload

---
+++ BracketMatch.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+class BracketMatch {
+    public static boolean bracketMatch(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {
+                    return false;  
+                }
+                
+                char openBracket = stack.pop();
+                if ((ch == ')' && openBracket != '(') ||
+                    (ch == '}' && openBracket != '{') ||
+                    (ch == ']' && openBracket != '[')) {
+                    return false; 
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Bracket List");
+        String str = sc.nextLine();
+        System.out.println("Your Bracket match is : " + bracketMatch(str));
+    }
+}
