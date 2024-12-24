0e86254fd936ce7c03e8a93119f7ab8a97033fcb
Saniya Atar <114806512+SaniyaAtar@users.noreply.github.com>
2024-12-24 18:10:49 +0530

    updated code

---
+++ MathP.java
@@ -0,0 +1,9 @@
+public class MathP {
+    
+    public static void main(String[] args) {
+        double a=Math.sqrt(4);
+
+        System.out.println(a);
+        System.out.println(Math.PI);
+    }
+}
